4278404cbc1b2190ecca9db02cbf5c4aeb9a494b
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2020-09-08 17:58:00 +0900

    [UPDATE] 98%

--- Android/app/src/main/java/com/project/hackathon/MainActivity.kt
+++ Android/app/src/main/java/com/project/hackathon/MainActivity.kt
@@ -6,11 +6,10 @@ import android.text.Html
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.hackathon.adapter.RcViewAdapter
-import com.project.hackathon.data.Check
-import com.project.hackathon.data.Meal
-import com.project.hackathon.data.Response
+import com.project.hackathon.data.*
 import com.project.hackathon.network.MealClient
 import com.project.hackathon.network.MealService
+import com.project.hackathon.network.WeatherClient
 import com.project.hackathon.network.WeatherService
 import com.project.hackathon.room.DataBase
 import com.project.simplecode.simIntentNoFinish
@@ -24,16 +23,22 @@ import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-    private val schoolID = "7240393"
-    private val officeCode = "D10"
+    val SCHOOL_ID = "7240393"
+    val OFFICE_CODE = "D10"
+
+    val LAT: Double = 35.663053
+    val LON: Double = 128.413726
+    val APP_ID: String = "09c8dfc52b7541d33c528d09a55e2c18"
+    val UNITS: String = "metric"
 
     private var checkList = ArrayList<Check>()
     private val rcViewAdapter = RcViewAdapter()
-    var checkDB : DataBase? = null
+    var checkDB: DataBase? = null
 
-    lateinit var mealAPI : MealService
-    lateinit var weatherAPI : WeatherService
-    lateinit var mealRetrofit : Retrofit
+    lateinit var mealAPI: MealService
+    lateinit var weatherAPI: WeatherService
+    lateinit var mealRetrofit: Retrofit
+    lateinit var weatherRetrofit: Retrofit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,35 +53,83 @@ class MainActivity : AppCompatActivity() {
         main_rcView.layoutManager = LinearLayoutManager(this)
         main_rcView.adapter = rcViewAdapter
 
-
+        weatherRetrofit = WeatherClient.getInstance()
         mealRetrofit = MealClient.getInstance()
+
         getMeal()
+        getSchedule()
+
+        getWeather()
 
         clickListener()
     }
 
-    private fun clickListener(){
+    private fun clickListener() {
         main_addItem.setOnClickListener {
             simIntentNoFinish(AddActivity::class.java)
         }
     }
-    //급식 정보를 받아오는 코드
-    fun getMeal(){
-        val now : Long = System.currentTimeMillis()
-        val date : Date = Date(now)
-        val dateFormat : SimpleDateFormat = SimpleDateFormat("yyyyMMdd")
-        val getTime :String = dateFormat.format(date)
 
-        Log.d("Logg", getTime)
+    fun getWeather() {
+        weatherAPI = weatherRetrofit.create(WeatherService::class.java)
+        weatherAPI.getWeather(LAT, LON, APP_ID, UNITS)
+            .enqueue(object : Callback<TotalWeather> {
+                override fun onResponse(
+                    call: Call<TotalWeather>,
+                    response: retrofit2.Response<TotalWeather>
+                ) {
+                    var icon = response.body()?.weatherList?.get(0)?.icon
+
+                    if (icon == "01d") {
+                        main_weather_img.setImageResource(R.drawable.sun)
+                        main_weather_comment.text = "아잇 눈부셔"
+                    } else if (icon == "01n") {
+                        main_weather_img.setImageResource(R.drawable.night)
+                        main_weather_comment.text = "소화도 할 겸 산책 어떤가요?"
+                    } else if (icon == "02d") {
+                        main_weather_img.setImageResource(R.drawable.sun_cloud)
+                        main_weather_comment.text = "오늘 야자는 강당이다"
+                    } else if (icon == "02n") {
+                        main_weather_img.setImageResource(R.drawable.night_cloud)
+                        main_weather_comment.text = "창문 열고 자면 추울 것 같아요"
+                    }else if(icon == "03d" || icon == "03n" || icon == "04d" || icon == "04n"){
+                        main_weather_img.setImageResource(R.drawable.cloud)
+                        main_weather_comment.text = "어두컴컴하니깐 텐션 떨어져요"
+                    } else if (icon == "09d" || icon == "09n" || icon == "10n" || icon == "10d") {
+                        main_weather_img.setImageResource(R.drawable.rain)
+                        main_weather_comment.text = "실내점호! 실내점호! 실내점호!"
+                    }else if(icon == "11d" || icon == "11n"){
+                        main_weather_img.setImageResource(R.drawable.thunder)
+                        main_weather_comment.text = "좀 더 격렬하게 실내점호! 실내점호!"
+                    }
+                    else if (icon == "13d" || icon == "13n") {
+                        main_weather_img.setImageResource(R.drawable.snow)
+                    }else if(icon == "50d" || icon == "50n"){
+                        main_weather_img.setImageResource(R.drawable.mist)
+                    }
+
+                    main_temp.text = String.format("%.1f", response.body()?.main?.temp) + "°C"
+                }
+
+                override fun onFailure(call: Call<TotalWeather>, t: Throwable) {
+                    Log.d("Logg", t.message)
+                }
+            })
+    }
+
+    //급식 정보를 받아오는 코드
+    fun getMeal() {
 
         mealAPI = mealRetrofit.create(MealService::class.java)
-        mealAPI.getMeal(schoolID, officeCode, getTime)
-            .enqueue(object : Callback<Response<Meal>>{
-                override fun onResponse(call: Call<Response<Meal>>, response: retrofit2.Response<Response<Meal>>) {
+        mealAPI.getMeal(SCHOOL_ID, OFFICE_CODE, today())
+            .enqueue(object : Callback<Response<Meal>> {
+                override fun onResponse(
+                    call: Call<Response<Meal>>,
+                    response: retrofit2.Response<Response<Meal>>
+                ) {
                     breakfast_menu.text = Html.fromHtml(response.body()?.data?.meals?.get(0))
                     lunch_menu.text = Html.fromHtml(response.body()?.data?.meals?.get(1))
                     dinner_menu.text = Html.fromHtml(response.body()?.data?.meals?.get(2))
-                    Log.d("Logg",response.body()?.data?.meals?.get(1))
                 }
 
                 override fun onFailure(call: Call<Response<Meal>>, t: Throwable) {
@@ -84,4 +137,38 @@ class MainActivity : AppCompatActivity() {
                 }
             })
     }
+
+    fun getSchedule() {
+        mealAPI = mealRetrofit.create(MealService::class.java)
+        mealAPI.getSchedule(SCHOOL_ID, OFFICE_CODE, today())
+            .enqueue(object : Callback<Response<Schedule>> {
+                override fun onResponse(
+                    call: Call<Response<Schedule>>,
+                    response: retrofit2.Response<Response<Schedule>>
+                ) {
+                    Log.d("Logg", response.body()?.status.toString())
+                    if (response.body()?.status == null) {
+                        main_schedule.text = "오늘은 학사일정이 없습니다."
+                    } else {
+                        main_schedule.text = response.body()?.data?.schedules?.get(0)?.name
+                    }
+
+                }
+
+                override fun onFailure(call: Call<Response<Schedule>>, t: Throwable) {
+                    Log.d("Logg", t.message)
+                }
+            })
+    }
+
+    fun today(): String {
+        var now: Long = System.currentTimeMillis()
+        var date: Date = Date(now)
+        Log.d("Logg", date.toString())
+        var dateFormat: SimpleDateFormat = SimpleDateFormat("yyyyMMdd")
+        var getTime: String = dateFormat.format(date)
+
+        return getTime
+    }
+
 }
