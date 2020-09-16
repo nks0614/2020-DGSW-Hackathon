034bb03573007cc45fe96a6cbaf0170ae08aec18
KimDongHee <donghee0901@naver.com>
2020-09-16 20:34:34 +0900

    Add show Schedule in ScheduleView

---
+++ Window/DGM_windows/DGM_windows/GetSchoolScheduleContent.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace DGM_windows
+{
+    class GetSchoolScheduleContent
+    {
+        public static List<getScheduleInfo.ScheduleInfo> getFixScheduleContent(string SelectDate)
+        {
+            List<getScheduleInfo.ScheduleInfo> returnResult = new List<getScheduleInfo.ScheduleInfo>();
+            WebClient web = new WebClient();
+            web.Encoding = Encoding.UTF8;
+            string url = string.Format("http://kyungwon-server.kro.kr:8080/schedule?school_id=7240393&office_code=D10&date={0}", SelectDate);
+
+            try
+            {
+                var json = web.DownloadString(url);
+
+                var result = JsonConvert.DeserializeObject<scheduleInfo.Root>(json);
+
+                scheduleInfo.Root outPut = result;
+
+                foreach(scheduleInfo.schedules getValue in outPut.data.schedules)
+                {
+                    getScheduleInfo.ScheduleInfo value = new getScheduleInfo.ScheduleInfo();
+                    value.id = -1;
+                    value.description = getValue.name;
+                    returnResult.Add(value);
+                }
+
+                return returnResult;
+            }
+            catch
+            {
+                return returnResult;
+            }
+        }
+        public static List<getScheduleInfo.ScheduleInfo> getDynamicScheduleContent(string SelectDate)
+        {
+            List<getScheduleInfo.ScheduleInfo> returnResult = new List<getScheduleInfo.ScheduleInfo>();
+            SqlConnection connect = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\user\\source\\repos\\DrawingBoard\\2020-DGSW-Hackathon\\Window\\DGM_windows\\DGM_windows\\Schedule.mdf;Integrated Security=True");
+            
+            try
+            {
+                connect.Open();
+                using (SqlCommand command = new SqlCommand(string.Format("SELECT Id,description FROM Schedule where time = '{0}'", SelectDate), connect))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        getScheduleInfo.ScheduleInfo value = new getScheduleInfo.ScheduleInfo();
+                        value.id = Int32.Parse(reader["Id"].ToString());
+                        value.description = reader["description"].ToString();
+                        returnResult.Add(value);
+                    }
+                }
+
+                connect.Close();
+
+                return returnResult;
+            }
+            catch
+            {
+                return returnResult;
+            }
+        }
+    }
+}
--- Window/DGM_windows/DGM_windows/ScheduleView.xaml.cs
+++ Window/DGM_windows/DGM_windows/ScheduleView.xaml.cs
@@ -23,6 +23,83 @@ namespace DGM_windows
         {
             InitializeComponent();
             SelectDate.SelectedDate = DateTime.Now;
+
+            List<getScheduleInfo.ScheduleInfo> FixValue = GetSchoolScheduleContent.getFixScheduleContent(MainWindow.Today);
+            List<getScheduleInfo.ScheduleInfo> DynamicValue = GetSchoolScheduleContent.getDynamicScheduleContent(MainWindow.Today);
+            if ((FixValue.Count + DynamicValue.Count) == 0)
+            {
+                ScheduleViewContent.Children.Add(MakeTextBox(1, "일정이 없습니다.", true));
+            }
+            else
+            {
+                foreach (getScheduleInfo.ScheduleInfo value in FixValue)
+                {
+                    ScheduleViewContent.Children.Add(MakeTextBox(value.id, value.description, true));
+                }
+                foreach (getScheduleInfo.ScheduleInfo value in DynamicValue)
+                {
+                    ScheduleViewContent.Children.Add(MakeTextBox(value.id, value.description, false));
+                }
+            }
+        }
+
+        private Grid MakeTextBox(int id, string description, bool isFix)
+        {
+            Grid grid = new Grid();
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.Fill = Brushes.White;
+            rectangle.Stroke = Brushes.Black;
+            rectangle.RadiusX = 5;
+            rectangle.RadiusY = 5;
+            rectangle.HorizontalAlignment = HorizontalAlignment.Center;
+            rectangle.VerticalAlignment = VerticalAlignment.Center;
+            rectangle.Height = 50;
+            rectangle.Width = 380;
+
+            Label testText = new Label();
+            testText.Background = Brushes.Transparent;
+            testText.Height = 50;
+            testText.Width = 380;
+            testText.FontSize = 20;
+            testText.Content = description;
+            testText.BorderBrush = Brushes.Transparent;
+            testText.VerticalContentAlignment = VerticalAlignment.Center;
+            testText.HorizontalContentAlignment = HorizontalAlignment.Center;
+            testText.VerticalAlignment = VerticalAlignment.Center;
+            testText.HorizontalAlignment = HorizontalAlignment.Center;
+
+            grid.Name = (string.Format("id{0}", id));
+            grid.Margin = new Thickness(0, 0, 0, 5);
+            grid.Children.Add(rectangle);
+            grid.Children.Add(testText);
+            if(isFix == false)
+            {
+                grid.MouseDown += TextView_MouseDown;
+            }
+            else
+            {
+                rectangle.Fill = Brushes.LightGray;
+            }
+
+            return grid;
+        }
+
+        private void Schedule_Closed(object sender, EventArgs e)
+        {
+            MainWindow.IsClose = 1;
+        }
+
+        private void CloseButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+        private void TextView_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var selectingName = sender as DependencyObject;
+            string name = selectingName.GetValue(Grid.NameProperty) as string;
+            name = name.Replace("id", "");
+            MessageBox.Show(name);
         }
     }
 }
